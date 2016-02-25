0aa704a01f1984184cad946063f1f446bebf2317
Natalia Podosinnikova <Natalia@natalias-air.attlocal.net>
2016-02-25 14:39:54 -0600

    search with rerouting functional

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 //Search Function
-var token = "?access_token=0be2cadc97096f2d2a04bbb9efdf2502dc188653"
+//var token = "?access_token=7f3238738f78bc7f0dd5b63cf8619a4692d6cc3f"
 
 var inputEl = document.querySelector('#search')
 inputEl.value = "Search GitHub"
 
 var searchBaseUrl = "https://api.github.com/users/"
 
-
-
-var doSearchProfileRequest = function(userName) {
-	var fullUrl = searchBaseUrl + userName + token
-	var userNamePromise = $.getJSON(fullUrl)  
+var doSearchRequest = function(userName) {
+	var profileUrl = searchBaseUrl + userName //+ token
+	var userNamePromise = $.getJSON(profileUrl)  
 	userNamePromise.then(showObj)
-} 
-
-var doSearchReposRequest = function(userName) {
-	var fullUrl = searchBaseUrl + userName + "/repos"+ token
-	var userReposPromise = $.getJSON(fullUrl)  
+	var reposUrl = searchBaseUrl + userName + "/repos"//+ token
+	var userReposPromise = $.getJSON(reposUrl)  
 	userReposPromise.then(showData)
 }
 
-
-var inputToUrl = function(keyEvent) {	
-  	var inputEl = keyEvent.target
-  		if (keyEvent.keyCode === 13) {
-      		var userName = inputEl.value
-      		inputEl.value = ""
-      		location.hash = userName
-      		doSearchProfileRequest(userName)
-      		doSearchReposRequest(userName)
-  }
+var inputToUrl = function(keyEvent) {
+    var inputEl = keyEvent.target
+    if (keyEvent.keyCode === 13) {
+        var userName = inputEl.value
+        inputEl.value = ""
+        location.hash = userName
+    }
 }
 
-inputEl.addEventListener('keydown', inputToUrl)
+inputEl.addEventListener("keydown", inputToUrl)
+
+var controller = function() {
+    var userName = location.hash.substring(1)
+    doSearchRequest(userName)
+}
+
+window.addEventListener("hashchange", controller)
 
 
 //Left Column 
@@ -41,7 +40,7 @@ var leftCol = document.querySelector("#left")
 
 var profileBaseUrl = "https://api.github.com/users/RedStarThrower"
 
-var profileUrl = profileBaseUrl + token
+var profileUrl = profileBaseUrl //+ token
 
 var profilePromise = $.getJSON(profileUrl)
 
@@ -54,16 +53,15 @@ var showObj = function(jsonObj) {
 }
 
 var objToHTML = function(jsonObj) {
-	var profileString = ""
-		profileString += '<img class="avatar" src="' + jsonObj.avatar_url + '">'
-		profileString += '<div class="namesBox">' + '<div class="full-name">' + jsonObj.name + '</div>'
-		profileString += '<div class="username">' + jsonObj.login + '</div>' + '</div>'
-		profileString += '<ul class="details">' + '<li>' + jsonObj.location + '</li>' 
-		profileString += '<li>'+ '<a href="mailto:nat.podo@gmail.com" target="_blank">nat.podo@gmail.com</a>' + '</li>' 
-		profileString += '<li>'+ '<a href="https://medium.com/@RedStarThrower" target="_blank">Blog</a>' + '</li>' 
-		profileString += '<li>' + '<time class="join-date" datetime="' + jsonObj.created_at + 'day="numeric" is="local-time" month="short" year="numeric" title="Feb 6, 2014, 11:21 PM CST">Joined on Feb 6, 2014</time>'
-		+ '</ul>'
-	return profileString
+    var profileString = ""
+    profileString += '<img class="avatar" src="' + jsonObj.avatar_url + '">'
+    profileString += '<div class="namesBox">' + '<div class="full-name">' + jsonObj.name + '</div>'
+    profileString += '<div class="username">' + jsonObj.login + '</div>' + '</div>'
+    profileString += '<ul class="details">' + '<li>' + jsonObj.location + '</li>'
+    profileString += '<li>' + '<a href="mailto:' + jsonObj.email + '" target="_blank">' + jsonObj.email + '</a>' + '</li>'
+    profileString += '<li>' + '<a href="' + jsonObj.blog + '" target="_blank">' + jsonObj.blog + '</a>' + '</li>'
+    profileString += '<li>' + '<time class="join-date" datetime="' + jsonObj.created_at + 'day="numeric" is="local-time" month="short" year="numeric" title="Feb 6, 2014, 11:21 PM CST">Joined on Feb 6, 2014</time>' + '</ul>'
+    return profileString
 }
 
 
@@ -76,7 +74,7 @@ var rightCol = document.querySelector("#right")
 
 var reposBaseUrl = "https://api.github.com/users/RedStarThrower/repos"
 
-var reposUrl = reposBaseUrl + token
+var reposUrl = reposBaseUrl //+ token
 
 var reposPromise = $.getJSON(reposUrl)
 
