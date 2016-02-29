1dd048f6d7fc55d79f74dab147dce1266146e4da
Natalia Podosinnikova <Natalia@natalias-air.attlocal.net>
2016-02-29 06:08:02 -0600

    search and styling

--- js/script.js
+++ js/script.js
@@ -1,19 +1,10 @@
 //Search Function
-//var token = "?access_token=7f3238738f78bc7f0dd5b63cf8619a4692d6cc3f"
+// var token = "?access_token=7f3238738f78bc7f0dd5b63cf8619a4692d6cc3f"
 
 var inputEl = document.querySelector('#search')
 inputEl.value = "Search GitHub"
 
 var searchBaseUrl = "https://api.github.com/users/"
-
-var doSearchRequest = function(userName) {
-	var profileUrl = searchBaseUrl + userName //+ token
-	var userNamePromise = $.getJSON(profileUrl)  
-	userNamePromise.then(showObj)
-	var reposUrl = searchBaseUrl + userName + "/repos"//+ token
-	var userReposPromise = $.getJSON(reposUrl)  
-	userReposPromise.then(showData)
-}
 
 var inputToUrl = function(keyEvent) {
     var inputEl = keyEvent.target
@@ -32,6 +23,16 @@ var controller = function() {
 }
 
 window.addEventListener("hashchange", controller)
+
+
+var doSearchRequest = function(userName) {
+	var profileUrl = searchBaseUrl + userName //+ token
+	var userNamePromise = $.getJSON(profileUrl)  
+	userNamePromise.then(showObj)
+	var reposUrl = searchBaseUrl + userName + "/repos" //+ token
+	var userReposPromise = $.getJSON(reposUrl)  
+	userReposPromise.then(showData)
+}
 
 
 //Left Column 
@@ -56,15 +57,13 @@ var objToHTML = function(jsonObj) {
     var profileString = ""
     profileString += '<img class="avatar" src="' + jsonObj.avatar_url + '">'
     profileString += '<div class="namesBox">' + '<div class="full-name">' + jsonObj.name + '</div>'
-    profileString += '<div class="username">' + jsonObj.login + '</div>' + '</div>'
+    profileString += '<div class="username">' + jsonObj.login + '</div>' + '<hr class="left-line">' + '</div>' 
     profileString += '<ul class="details">' + '<li>' + jsonObj.location + '</li>'
     profileString += '<li>' + '<a href="mailto:' + jsonObj.email + '" target="_blank">' + jsonObj.email + '</a>' + '</li>'
     profileString += '<li>' + '<a href="' + jsonObj.blog + '" target="_blank">' + jsonObj.blog + '</a>' + '</li>'
-    profileString += '<li>' + '<time class="join-date" datetime="' + jsonObj.created_at + 'day="numeric" is="local-time" month="short" year="numeric" title="Feb 6, 2014, 11:21 PM CST">Joined on Feb 6, 2014</time>' + '</ul>'
+    profileString += '<li>' + '<time class="join-date" datetime="' + jsonObj.created_at + 'day="numeric" is="local-time" month="short" year="numeric" title="Feb 6, 2014, 11:21 PM CST">Joined on Feb 6, 2014</time>' + '</ul>' + '<hr class="left-line">'
     return profileString
 }
-
-
 
 profilePromise.then(showObj)
 
@@ -90,15 +89,23 @@ var showData = function(jsonArray) {
 
 
 var arrToHTML = function(repoObj) {
-	var repoString = '<div class="repo-container">'
+	var repoString = '<p id="repos-title">Repositories</p>' + '<div class="repo-container">'
 		repoString += '<h3 class="repo-name">' + '<a href="' + repoObj.html_url + '" target="_blank">' + repoObj.name + '</a>' + '</h3>'
 		repoString += '<p class="repo-description">' + repoObj.description + '</p>'	
-		repoString += '<p class="repo-list-meta">Updated: <time datetime="'+ repoObj.updated_at +  'is="relative-time" title="Feb 16, 2016, 10:37 PM CST">8 days ago</time>' + '</p>'
+		repoString += '<p class="repo-list-meta">Updated: <time datetime="'+ repoObj.updated_at +  'is="relative-time" title="Feb 26, 2016, 10:37 PM CST">4 days ago</time>' + '</p>' + '<hr id="repo-line">'
 	+ '</div' 
  	return repoString
 }
 
 reposPromise.then(showData)
 
+//
 
+// if (location.hash !==''){
+//         controller()
+// }
+// else{
+// profilePromise.then(showObj)
+// reposPromise.then(showData)
+// }
 
